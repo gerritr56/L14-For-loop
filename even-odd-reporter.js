320efc12acbed1a24e53ff8a3f2e13dd0dc28373
gerritr56 <68347062+gerritr56@users.noreply.github.com>
2020-12-08 13:36:49 +0100

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,10 @@
+for (age = 0; age <20; age++) {
+
+if(age % 2 == 0) 
+    console.log(age +" is even");
+
+else 
+    console.log(age +" is oneven");
+}
+
+
---
+++ multiplication-tables.js
@@ -0,0 +1,6 @@
+
+for (number = 1; number <=10; number++){ 
+    for (tafel = 1; tafel <=10; tafel++) {
+    console.log(tafel + ' x '+ number + ' = ' + tafel * number);
+    }
+}
